d360dc53a91fdf39855ce6916c7c81b582c3a7d0
Quiros Cristian <quiroscristian12@gmail.com>
2023-11-05 16:03:08 -0300

    agrego bucle en js

--- js/index.js
+++ js/index.js
@@ -6,15 +6,20 @@ function solicitarNombreDeUsuario() {
     alert(devolucion);
 }
 
+solicitarNombreDeUsuario();
 
-let vehiculo = prompt("¿Cual es la marca de su vehiculo? volkswagen/chevrolet/ford")
+let continuar;
+
+do{
+
+    let vehiculo = prompt("¿Cual es la marca de su vehiculo? volkswagen/chevrolet/ford")
 
 
 
 if (vehiculo === "volkswagen") {
     alert("su vehiculo es un volkswagen");
 
-    let modelo = prompt("¿cual es el modelo de su vehiculo gol/ciroco");
+    let modelo = prompt("¿cual es el modelo de su vehiculo? gol/ciroco");
 
     if (modelo === "gol") {
         alert("el costo del gol es de $10.000 por mes");
@@ -25,7 +30,7 @@ if (vehiculo === "volkswagen") {
 } else if (vehiculo === "chevrolet") {
     alert("su vehiculo es un chevrolet");
 
-    let modelo = prompt("¿cual es el modelo de su vehiculo corvete/f100");
+    let modelo = prompt("¿cual es el modelo de su vehiculo? corvete/f100");
 
     if (modelo === "corvete") {
         alert("el costo del corvete es de $40.000 por mes");
@@ -36,7 +41,7 @@ if (vehiculo === "volkswagen") {
 } else if (vehiculo === "ford") {
     alert("su vehiculo es un ford");
 
-    let modelo = prompt("¿cual es el modelo de su vehiculo focus/mustang");
+    let modelo = prompt("¿cual es el modelo de su vehiculo? focus/mustang");
 
     if (modelo === "focus") {
         alert("el costo del focus es de $10.000 por mes");
@@ -46,7 +51,7 @@ if (vehiculo === "volkswagen") {
 }
 else {
     alert("no encuentro su vehiculo");
-    continuar = "si";
 }
 
-solicitarNombreDeUsuario();
+   continuar = prompt ("¿desea realizar una nueva cotizacion? SI/NO");
+}while(continuar === "si");
